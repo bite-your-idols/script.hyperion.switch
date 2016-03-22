535bc57f48d190b61d8d029a9e4f0ad5a2f0a195
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-03-22 10:17:18 +0100

    Delete addon.py

--- script.hyperion.switch/addon.py
+++
@@ -1,8 +0,0 @@
-import xbmcaddon
-import xbmcgui
-import os
- 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
-
-os.system("/storage/.kodi/addons/script.hyperion.switch/bin/switch.sh")
