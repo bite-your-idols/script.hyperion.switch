7a3a0888d9ae505c86680b8987a6bbd22dd92ed3
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-03-25 16:47:26 +0100

    Update switch.sh

--- bin/switch.sh
+++ bin/switch.sh
@@ -4,8 +4,8 @@ SERVICE='hyperiond'
 if ps | grep -v grep | grep $SERVICE > /dev/null
 then
 killall hyperiond
-curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Hyperion","message":"Disabled","image":"/storage/.kodi/addons/script.hyperion.switch/icon.png"}}' http://192.168.0.14:80/jsonrpc
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Hyperion","message":"Disabled","image":"/storage/.kodi/addons/script.hyperion.switch-master/icon.png"}}' http://192.168.0.14:80/jsonrpc
 else
 /storage/hyperion/bin/hyperiond.sh /storage/.config/hyperion.config.json </dev/null >/dev/null 2>&1 &
-curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Hyperion","message":"Enabled","image":"/storage/.kodi/addons/script.hyperion.switch/icon.png"}}' http://192.168.0.14:80/jsonrpc
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Hyperion","message":"Enabled","image":"/storage/.kodi/addons/script.hyperion.switch-master/icon.png"}}' http://192.168.0.14:80/jsonrpc
 fi
