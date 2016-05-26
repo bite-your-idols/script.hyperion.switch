5ed53ee0eff4adb2ab098fa1f235aa7cec61edbd
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-05-26 11:47:39 +0200

    Update addon.py

--- addon.py
+++ addon.py
@@ -9,4 +9,4 @@ addonname   = addon.getAddonInfo('name')
 script_file = os.path.realpath(__file__)
 directory = os.path.dirname(script_file)
 
-os.system("sh "+directory+"bin/switch.sh "+directory)
+os.system("sh "+directory+"/bin/switch.sh "+directory)
