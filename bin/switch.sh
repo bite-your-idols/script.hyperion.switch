487661168aeb0b23ee9cfc205b2955c1c84f2c25
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2017-02-23 18:56:08 +0100

    ip fix
    https://github.com/bite-your-idols/script.hyperion.switch/issues/1

--- bin/switch.sh
+++ bin/switch.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 ADDON_DIRECTORY=$1
 # chmod a+x $1/bin/*
+KODIIP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 SERVICE='hyperiond'
 if ps | grep -v grep | grep $SERVICE > /dev/null
 then
 killall hyperiond
-curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Hyperion","message":"Disabled","image":"/storage/.kodi/addons/script.hyperion.switch-master/icon.png"}}' http://192.168.0.14:80/jsonrpc
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Hyperion","message":"Disabled","image":"/storage/.kodi/addons/script.hyperion.switch-master/icon.png"}}' http://$KODIIP:8080/jsonrpc
 else
 /storage/hyperion/bin/hyperiond.sh /storage/.config/hyperion.config.json </dev/null >/dev/null 2>&1 &
-curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Hyperion","message":"Enabled","image":"/storage/.kodi/addons/script.hyperion.switch-master/icon.png"}}' http://192.168.0.14:80/jsonrpc
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"id":1,"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"Hyperion","message":"Enabled","image":"/storage/.kodi/addons/script.hyperion.switch-master/icon.png"}}' http://$KODIIP:8080/jsonrpc
 fi
