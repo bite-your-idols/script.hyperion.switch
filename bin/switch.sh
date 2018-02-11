506eca69bb569ee6b09b2551918053f7da69d196
chromoxdor <33860956+chromoxdor@users.noreply.github.com>
2018-02-11 16:45:16 +0100

    Update switch.sh
    I used the commands from hypercon.... works in openelec  8.0.4 and should work in other versions as well

--- bin/switch.sh
+++ bin/switch.sh
@@ -4,13 +4,9 @@ ADDON_DIRECTORY=$1
 PROCESS_NUM=`pgrep hyperiond | wc -l`
 if [ $PROCESS_NUM -eq 1 ]
 then
-# killall hyperiond
-# mv /storage/.kodi/addons/service.hyperion/ /storage/.kodi/addons/service.hyperion_OFF/
-systemctl stop service.hyperion.service 2>/dev/null
+killall hyperiond 2>/dev/null
 kodi-send --action=Notification"(Hyperion,Disabled,2000,/storage/.kodi/addons/script.hyperion.switch/icon.png)"
 else
-systemctl start service.hyperion.service 2>/dev/null
-# mv /storage/.kodi/addons/service.hyperion_OFF/ /storage/.kodi/addons/service.hyperion/
-# /storage/.kodi/addons/service.hyperion/bin/hyperiond.start /storage/.kodi/userdata/addon_data/service.hyperion/hyperion.config.json > /storage/.kodi/temp/hyperion.log 2>&1 &
+/storage/.config/autostart.sh > /dev/null 2>&1 &
 kodi-send --action=Notification"(Hyperion,Enabled,2000,/storage/.kodi/addons/script.hyperion.switch/icon.png)"
 fi
